169fa004eca4b91f90e801c93b84e3277c38c2e3
fanazz <pskirma@gmail.com>
2024-01-30 12:46:42 +0200

    duomenu baziu atnaujinimas

--- PTU20_live/peter db/uzduoties16-2-3 duomenys.sql
+++ PTU20_live/peter db/uzduoties16-2-3 duomenys.sql
@@ -1,1 +1,9 @@
 -- SQLite
+--Sukurkite duomenų bazę, kurioje saugosite informaciją apie studentų duomenis
+--ir jų kursus. Kiekvienas studentas gali mokytis daugelyje kursų,
+--ir kiekvienas kursas gali turėti daug studentų. 
+--Sukurkite atitinkamas lentelės ir susiekite jas Many-to-Many ryšiu.
+
+CREATE TABLE students (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+
